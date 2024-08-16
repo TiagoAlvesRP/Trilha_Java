de4418ef5282b6401fcb009807ec580006aebd4e
perkles <astiagoalves@hotmail.com>
2024-08-16 00:04:33 -0300

    novos projetos trilha

---
+++ Controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        double nota = 10;
+        double mediaAprovacao = 8;
+        
+        String Resultado = nota >= mediaAprovacao ? "aprovado" : "reprovado";
+
+        System.out.println(Resultado);        
+
+        //if(nota >= mediaAprovacao)
+        //  System.out.println("Aprovado");        
+        //else
+        //  System.out.println("Reprovado"); 
+          
+          
+           
+    }
+    
+}
